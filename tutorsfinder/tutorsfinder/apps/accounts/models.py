d1f48c4b922e144820273027392148efef60720a
Dedi Purwanto <kecebongsoft@gmail.com>
2012-12-08 20:52:25 +0800

    Added ResetPasswordRequest and ValidationStatus model

--- tutorsfinder/tutorsfinder/apps/accounts/models.py
+++ tutorsfinder/tutorsfinder/apps/accounts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ResetPasswordRequest(models.Model):
+
+    user = models.ForeignKey(User, related_name='reset_password_requests')
+    token = models.CharField(max_length=1024)
+    used = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+class ValidationStatus(models.Model):
+
+    user = models.OneToOneField(User, related_name='validation_status')
+    token = models.CharField(max_length=1024)
+    validated = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        verbose_name_plural = "Validation Statuses"
