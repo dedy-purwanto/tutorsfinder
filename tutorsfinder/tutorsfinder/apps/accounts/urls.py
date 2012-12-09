bee13886df8b6637ebfc911c4133b6a88ca099c8
Dedi Purwanto <kecebongsoft@gmail.com>
2012-12-09 11:41:26 +0800

    restructuring accounts urls

--- tutorsfinder/tutorsfinder/apps/accounts/urls.py
+++ tutorsfinder/tutorsfinder/apps/accounts/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls.defaults import patterns, url
 
-from .views import LoginView, RegisterView, ValidateAccountView,\
-    ForgotPasswordView, ForgotPasswordSuccessView, LogoutView
+from .views import RegisterView, ValidateAccountView,\
+    ForgotPasswordView, ForgotPasswordSuccessView
 
 urlpatterns = patterns('accounts.views',
-    url(r'^login/$', LoginView.as_view(), name='login'),
-    url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^forgot-password/$', ForgotPasswordView.as_view(), name='forgot_password'),
     url(r'^forgot-password-success/$', ForgotPasswordSuccessView.as_view(), name='forgot_password_success'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
--- tutorsfinder/tutorsfinder/urls.py
+++ tutorsfinder/tutorsfinder/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from accounts.views import LoginView, LogoutView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'', include('home.urls', namespace='home')),
-    url(r'', include('accounts.urls', namespace='accounts')),
+
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+
     url(r'^admin/', include(admin.site.urls)),
 )
