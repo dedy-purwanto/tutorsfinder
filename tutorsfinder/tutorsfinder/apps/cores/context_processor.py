888eaf563e245464a518ab81b5cd3e0098de4444
Dedi Purwanto <kecebongsoft@gmail.com>
2012-12-09 09:03:26 +0800

    Fetch site name, fix wrong block placement.

--- tutorsfinder/tutorsfinder/apps/cores/context_processor.py
+++ tutorsfinder/tutorsfinder/apps/cores/context_processor.py
@@ -1,5 +1,6 @@
+from django.contrib.sites.models import Site
 def base_context(request):
-    site_name = 'Tutorsfinder'
+    site_name = Site.objects.all()[0].name
 
     return {
         'SITE_NAME' : site_name,
