946426f7d6a7850bf0e1c968a094d26d2ea08d19
Dedi Purwanto <kecebongsoft@gmail.com>
2012-12-12 12:14:16 +0800

    Added flatpages catchall patterns

--- tutorsfinder/tutorsfinder/urls.py
+++ tutorsfinder/tutorsfinder/urls.py
@@ -21,6 +21,9 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+urlpatterns += patterns('django.contrib.flatpages.views',
+    (r'^(?P<url>.*)$', 'flatpage'),
+)
 
 if settings.DEBUG:
     urlpatterns += patterns('',
